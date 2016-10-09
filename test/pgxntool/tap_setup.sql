cbbcb0e3a6157111f9fca2e3727e0ac535a3291b
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-10-09 17:47:32 -0500

    Squashed 'pgxntool/' changes from 48dce67..cd24b0f
    cd24b0f Reduce verbosity of test setup
    
    git-subtree-dir: pgxntool
    git-subtree-split: cd24b0f8514143c3607094602eca0869ce4979d2

--- test/pgxntool/tap_setup.sql
+++ test/pgxntool/tap_setup.sql
@@ -1,8 +1,10 @@
 \i test/pgxntool/psql.sql
 
+SET client_min_messages = WARNING;
 CREATE SCHEMA IF NOT EXISTS tap;
 SET search_path = tap, public;
 CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
+SET client_min_messages = NOTICE;
 \pset format unaligned
 \pset tuples_only true
 \pset pager
